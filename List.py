ebfb700c6cc13f84c0faad394fda70561bdcd7f8
Mahdi <beitollahi.mahdi@gmail.com>
2022-06-19 18:56:59 -0400

    added suduku and binary serach

--- List.py
+++ List.py
@@ -283,7 +283,41 @@ def search(nums, target: int) -> int:
                 r = mid - 1
     
     return -1
-            
+
+def searchRange(nums, target: int):
+    """
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+    If target is not found in the array, return [-1, -1].
+    
+    You must write an algorithm with O(log n) runtime complexity.
+    
+    Example 1:
+    
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
+    
+    """
+    if not nums:
+        return [-1, -1]
+    
+    l, r = 0, len(nums) - 1
+    while (l <= r):
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            i = j = mid 
+            while i >= 1 and nums[i - 1] == target:
+                i -= 1
+            while j <= len(nums)-2 and nums[j + 1] == target:
+                j += 1
+            return [i, j]
+        
+        if nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return [-1, -1]
+
 
 if __name__=="__main__":
     sample=[1,2,9, 3,3,4,5,6,6,7,8,9,1,3,3]
--- Other.py
+++ Other.py
@@ -56,6 +56,57 @@ def reverse( x: int) -> int:
     out = int(out) if pos else -1 * int(out)
     return out
 
+def isValidSudoku(board) -> bool:
+    """
+    Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+    Note:
+    
+    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    Only the filled cells need to be validated according to the mentioned rules.
+    
+    Input: board = 
+    [["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]]
+    Output: true
+
+    """
+    
+    dic_column = {} # (col, cell): None
+    
+    for row_n, row in enumerate(board):
+        dic_row = {} # cell: None
+        if row_n % 3 == 0:
+             dic_box = {} # (box, cell): None
+                
+        for col, cell in enumerate(row):
+            if cell != "." and cell in dic_row:
+                return False
+            else:
+                dic_row[cell] = None
+            
+            box = col // 3
+            if cell != "." and (box, cell) in dic_box:
+                return False
+            else:
+                dic_box[(box, cell)] = None
+                
+            if cell != "." and (col, cell) in dic_column:
+                return False
+            else:
+                dic_column[(col, cell)] = None
+    return True
+
 if __name__=="__main__":
     num=8
     print(PrimeFac(num))
